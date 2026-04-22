1974bef227d7a3d3bcabc1ceba8e025c63aa35e4
cayo <cayofonseca12@gmail.com>
2026-04-22 15:40:01 -0300

    --amend

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filter/global.exception';
+import { GlobalInterceptor } from './common/interceptors/global.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new GlobalInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
